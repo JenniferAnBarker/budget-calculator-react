7910d73a7dcb617207cd177f1fc95564085d2bb8
Jennifer N-B <Jenniebarker1o1@gmail.com>
2022-11-22 21:13:20 +0000

    most functions made, no more warrnings

--- src/App.js
+++ src/App.js
@@ -12,15 +12,97 @@ const initialExpenses = [
 ];
 
 function App() {
+  // ****** state values ********
+  // all expenses, add expense
   const [expenses, setExpenses] = useState(initialExpenses);
+  // single expense
+  const [charge, setCharge] = useState("");
+  // single amount
+  const [amount, setAmount] = useState("");
+  // ****** functionality ********
+  const [alert, setAlert] = useState({ show: false });
+
+  const [edit, setEdit] = useState(false);
+
+  const [id, setId] = useState(0);
+
+  const handleCharge = (e) => {
+    setCharge(e.target.value);
+  };
+  const handleAmount = (e) => {
+    setAmount(e.target.value);
+  };
+  const handleAlert = ({ type, text }) => {
+    setAlert({ show: true, type, text });
+    setTimeout(() => {
+      setAlert({ show: false });
+    }, 5000);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (charge !== "" && amount > 0) {
+      if (edit) {
+        let tempExpenses = expenses.map((item) => {
+          return item.id === id ? { ...item, charge, amount } : item;
+        });
+        setExpenses(tempExpenses);
+        setEdit(false);
+      } else {
+        const singleExpense = { id: uuidv4(), charge, amount };
+        setExpenses([...initialExpenses, singleExpense]);
+        handleAlert({ type: "success", text: "Item added!" });
+      }
+
+      setCharge("");
+      setAmount("");
+    } else {
+      handleAlert({
+        type: "danger",
+        text: `Please type an amount greater than zero`,
+      });
+    }
+  };
+
+  const clearItems = () => {
+    setExpenses([]);
+    handleAlert({ type: `danger`, text: `all items deleted` });
+  };
+
+  const handleDelete = (id) => {
+    let tempExpenses = expenses.filter((item) => item.id !== id);
+    setExpenses(tempExpenses);
+    handleAlert({ type: `danger`, text: `item deleted` });
+  };
+
+  const handleEdit = (id) => {
+    let expense = expenses.find((item) => item.id === id);
+    let { charge, amount } = expense;
+    setCharge(charge);
+    setAmount(amount);
+    setEdit(true);
+    setId(id);
+  };
 
   return (
     <>
-      <Alert />
+      {alert.show && <Alert type={alert.type} text={alert.text} />}
       <h1>Budget Calculator</h1>
       <main className="App">
-        <Form />
-        <List expenses={expenses} />
+        <Form
+          charge={charge}
+          amount={amount}
+          handleAmount={handleAmount}
+          handleCharge={handleCharge}
+          handleSubmit={handleSubmit}
+          edit={edit}
+        />
+        <List
+          expenses={expenses}
+          handleDelete={handleDelete}
+          handleEdit={handleEdit}
+          clearItems={clearItems}
+        />
       </main>
       <h1>
         {" "}
@@ -28,7 +110,7 @@ function App() {
         <span className="total">
           $
           {expenses.reduce((accumulator, current) => {
-            return (accumulator += current.amount);
+            return (accumulator += parseInt(current.amount));
           }, 0)}
         </span>
       </h1>
